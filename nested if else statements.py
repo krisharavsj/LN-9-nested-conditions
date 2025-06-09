1fa17b7dcf6baf2c3266f44d325ba689a54c8b43
krisharavsj <krisharsv@gmail.com>
2025-06-09 20:30:57 +0530

    first commit

---
+++ nested if else statements.py
@@ -0,0 +1,9 @@
+attendance = int(input("please enter your attendance"))
+if attendance>75:
+    print("student is allowed")
+if attendance<75:
+    medical=(input("please enter if you have any medical cause"))
+    if medical ==True:
+         print ("the student is allowed")
+    else:
+        print("the student is not allowed")
