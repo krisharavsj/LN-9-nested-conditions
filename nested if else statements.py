7497e22f070eb8d5c9a24d02743ddd4a3dc5152b
krisharavsj <krisharsv@gmail.com>
2025-06-09 20:41:10 +0530

    nested if else.py

--- nested if else statements.py
+++ nested if else statements.py
@@ -1,6 +1,6 @@
 attendance = int(input("please enter your attendance"))
-if attendance>75:
-    print("student is allowed")
+if attendance>=75:
+        print("student is allowed")
 if attendance<75:
     medical=(input("please enter if you have any medical cause"))
     if medical ==True:
